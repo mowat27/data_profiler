c771a8b5df4e23166f4892df66e90b414ead4453
Adrian Mowat <adrian.mowat@gmail.com>
2014-05-22 21:16:31 +0100

    New project

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject data_profiler "0.1.0-SNAPSHOT"
+  :description "A data profiler written in Clojure"
+  :url "https://github.com/mowat27/data_profiler"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
