6edcb0bababe2ea77f3edce61ed050c826fd6ad0
Adrian Mowat <adrian.mowat@gmail.com>
2014-05-22 22:44:04 +0100

    Codify formats for a flat data set

--- src/data_profiler/profiler.clj
+++ src/data_profiler/profiler.clj
@@ -12,8 +12,15 @@
   (apply merge (for [field (fields coll)]
                  {field (mapv field coll)})))
 
-
-
+(defn codify-format [x] 
+  (if (nil? x)
+    nil
+    (str/join 
+     (map (fn [c] (let [s (str c)] 
+                    (cond (re-matches #"[a-z]" s) "a"
+                          (re-matches #"[0-9]" s) "n"
+                          (re-matches #"[A-Z]" s) "A"
+                          :else s))) (str x)))))
 
 
 
