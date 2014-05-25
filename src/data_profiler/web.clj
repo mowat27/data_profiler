6c67c8bbabc527094f82f365206732ddc61cd01d
Adrian Mowat <adrian.mowat@gmail.com>
2014-05-26 00:05:15 +0100

    Basic format rendering

--- src/data_profiler/web.clj
+++ src/data_profiler/web.clj
@@ -4,11 +4,15 @@
             [bidi.bidi :as bidi]
             [clostache.parser :refer (render-resource)]
             [data-profiler.csv :as csv]
-            [data-profiler.profiler :as profiler]))
+            [data-profiler.profiler :as profiler]
+            [clojure.pprint :refer (pprint)]))
+
+(def available-files csv/examples)
 
 (defn using-layout [layout handler & {:keys [status]}]
   (let [response {:status (or status 200)}
-        layouts {:application "views/layouts/application.mustache"}]
+        layouts {:application "views/layouts/application.mustache"
+                 :dashboard "views/layouts/dashboard.mustache" }]
     (fn [req] 
       (assoc response :body (render-resource (layout layouts) 
                                              {:content (handler req)})))))
@@ -16,7 +20,42 @@
 (defn index [] 
   (using-layout :application (fn [_] "<p>Hello World</p>")))
 
-(def router (bidi/make-handler ["/" {"index.html" (index)}]))
+(defn render-formats [field rows]
+  (->> rows
+       (map (comp profiler/codify-format field))
+       frequencies
+       (map #(hash-map :format (let [x (first %)]
+                                 (cond (nil? x) "nil"
+                                       (empty? x) "\"\""
+                                       :else x)) 
+                       :count (second %)))
+       (sort-by :count)
+       reverse))
+
+(defn show-profile [req]
+  (let [source (-> req :route-params :file-name)
+        uri (get available-files (keyword source))
+        rows (when uri (csv/parse uri))] 
+    (if rows
+      {:status 200 
+       :body (render-resource "views/layouts/dashboard.mustache"
+                              {:name source
+                               :source uri
+                               :row-count (count rows)
+                               :base-profile (profiler/base-profile rows)
+                               :fields (for [field (profiler/fields rows)]
+                                         {:name    (name field)
+                                          :formats (render-formats field rows)})})}
+      {:status 404 
+       :bosy (format "<h1>Cannot find and data for %s</h1>" source)})))
+
+(->> (:elements available-files) 
+     csv/parse 
+     (map (comp profiler/codify-format :element))
+     (map #(hash-map :format %)))
+
+(def router (wrap-reload (bidi/make-handler ["/" {"index.html" (index)
+                                      ["profile/" :file-name] show-profile}])))
 
 (defn -main [& args] 
   (let [port (or (first args) "8080")] 
